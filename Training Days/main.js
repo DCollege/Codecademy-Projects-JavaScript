1adebad76435fd2fcafc78985c136c348d609e95
Donna <56034006+DCollege@users.noreply.github.com>
2020-07-24 16:05:10 -0400

    Create main.js

---
+++ Training Days/main.js
@@ -0,0 +1,52 @@
+//declaring types of events and selecting at random from an array of events
+const getRandEvent = () => {
+  const random = Math.floor(Math.random() * 3);
+  if (random === 0) {
+    return 'Marathon';
+  } else if (random === 1) {
+    return 'Triathlon';
+  } else if (random === 2) {
+    return 'Pentathlon';
+  }
+};
+
+//returns the number of days to train, based on the event selected
+const getTrainingDays = event => {
+  let days
+
+  if (event === 'Marathon') {
+    days = 50;
+  } else if (event === 'Triathlon') {
+    days = 100;
+  } else if (event === 'Pentathlon') {
+    days = 200;
+  }
+
+  return days;
+};
+
+const name = 'Nala';
+
+
+const logEvent = (name, event) => {
+  console.log(`${name}'s event is: ${event}`);
+};
+
+const logTime = (name, days) => {
+  console.log(`${name}'s time to train is: ${days} days`);
+};
+
+//calling the program
+const event = getRandEvent();
+const days = getTrainingDays(event); 
+
+
+logEvent(name, event);
+logTime(name, days);
+
+const event2 = getRandEvent();
+const days2 = getTrainingDays(event2);
+const name2 = 'Warren';
+
+logEvent(name2, event2);
+logTime(name2, days2);
